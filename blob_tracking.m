1453bd77bbdeb545f51ad2394f3f2610afafce6f
Gabriella G. Suarez <tuf00573@temple.edu>
2016-09-30 14:17:18 -0400

    cleaned up code and added ros communication

--- blob_tracking.m
+++ blob_tracking.m
@@ -1,11 +1,4 @@
 clc; clear;
-% Create the face detector object.
-blobDetector = vision.BlobAnalysis('BoundingBoxOutputPort', true, ...
-            'AreaOutputPort', true, 'CentroidOutputPort', true, ...
-            'MinimumBlobArea', 20000, 'MaximumBlobArea', 50000);
-
-% Create the point tracker object.
-pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
 
 % Create the webcam object.
 cam = webcam('FaceTime HD Camera (Built-in)');
@@ -17,103 +10,78 @@ frameSize = size(videoFrame);
 % Create the video player object.
 videoPlayer = vision.VideoPlayer('Position', [100 100 [frameSize(2), frameSize(1)]+30]);
 
+% set the loop to run unconditionally
 runLoop = true;
+
+% initialize the number of points
 numPts = 0;
+
+% count the number of frames
 frameCount = 0;
 
+% set up master node on global network
+rosinit;
+
+% create ros node on the network
+node = robotics.ros.Node('/test_node_1');
+
+% create publisher
+testpub = rospublisher('/rostest','std_msgs/String');
+
+% run while loop for 400 frames
 while runLoop && frameCount < 400
 
     % Get the next frame.
     videoFrame = snapshot(cam);
+    
+    % conver the frame to grayscale
     videoFrameGray = rgb2gray(videoFrame);
-    videoFrameCanny = im2bw(videoFrameGray, 0.3);
+    
+    % convert the frame to a binary image
+    videoFrameBin = im2bw(videoFrameGray, 0.3);
+    
+    % reduce the size of the frame
     videoFrame = imresize(videoFrame, 0.7);
     
-     
+    % increment the frame count  
     frameCount = frameCount + 1;
-
-    if numPts < 10
-        % Detection mode.
-        [area, centroid, bbox] = step(blobDetector, videoFrameCanny);
-        
-        [centers, radii] = imfindcircles(videoFrame,[100 150],'ObjectPolarity','dark', ...
-                                        'Sensitivity',0.95);
-        if ~isempty(centers)
-            bbox = [centers(1,2) - radii(1) centers(1,1) + radii(1) centers(1,1) - radii(1) centers(1,2) - radii(1)];
-            pixelValue = impixel(videoFrame, centers(1), centers(2))./255;
-            color = rgb2name(pixelValue);
-            videoFrame = insertShape(videoFrame, 'Filledcircle', [centers, radii(1)], 'color', 'white',...
-                                      'opacity', 0.4);
-            display(color);
-        end
-        
-           
+    
+    % find the center and the radii of the circular object
+    [centers, radii] = imfindcircles(videoFrame,[100 150],'ObjectPolarity','dark', ...
+                                        'Sensitivity',0.98);
+    % if the program detects a circular object
+    if ~isempty(centers)
             
-        
-        
-%         if ~isempty(bbox)
-%             % Find corner points inside the detected region.
-%             points = detectMinEigenFeatures(videoFrameCanny, 'ROI', bbox(1, :));
-% 
-%             % Re-initialize the point tracker.
-%             xyPoints = points.Location;
-%             numPts = size(xyPoints,1);
-%             release(pointTracker);
-%             initialize(pointTracker, xyPoints, videoFrameGray);
-% 
-%             % Save a copy of the points.
-%             oldPoints = xyPoints;
-% 
-%             % Convert the rectangle represented as [x, y, w, h] into an
-%             % M-by-2 matrix of [x,y] coordinates of the four corners. This
-%             % is needed to be able to transform the bounding box to display
-%             % the orientation of the face.
-%             bboxPoints = bbox2points(bbox(1, :));
-% 
-%             % Convert the box corners into the [x1 y1 x2 y2 x3 y3 x4 y4]
-%             % format required by insertShape.
-%             bboxPolygon = reshape(bboxPoints', 1, []);
-% 
-%             % Display a bounding box around the detected face.
-%             videoFrame = insertShape(videoFrame, 'Polygon', bboxPolygon, 'LineWidth', 3);
-% 
-%             % Display detected corners.
-%             videoFrame = insertMarker(videoFrame, xyPoints, '+', 'Color', 'white');
-%         end
-% 
-%     else
-%         % Tracking mode.
-%         [xyPoints, isFound] = step(pointTracker, videoFrameGray);
-%         visiblePoints = xyPoints(isFound, :);
-%         oldInliers = oldPoints(isFound, :);
-% 
-%         numPts = size(visiblePoints, 1);
-% 
-%         if numPts >= 10
-%             % Estimate the geometric transformation between the old points
-%             % and the new points.
-%             [xform, oldInliers, visiblePoints] = estimateGeometricTransform(...
-%                 oldInliers, visiblePoints, 'similarity', 'MaxDistance', 4);
-% 
-%             % Apply the transformation to the bounding box.
-%             bboxPoints = transformPointsForward(xform, double(bboxPoints));
-% 
-%             % Convert the box corners into the [x1 y1 x2 y2 x3 y3 x4 y4]
-%             % format required by insertShape.
-%             bboxPolygon = reshape(bboxPoints', 1, []);
-% 
-%             % Display a bounding box around the face being tracked.
-%             videoFrame = insertShape(videoFrame, 'Polygon', bboxPolygon, 'LineWidth', 3);
-% 
-%             % Display tracked points.
-%             videoFrame = insertMarker(videoFrame, visiblePoints, '+', 'Color', 'white');
-% 
-%             % Reset the points.
-%             oldPoints = visiblePoints;
-%             setPoints(pointTracker, oldPoints);
-%         end
+        % calculate the bounding boxes
+        bbox = [centers(1,2) - radii(1) centers(1,1) + radii(1) centers(1,1) - radii(1) centers(1,2) - radii(1)];
+            
+        % calculate the pixel value of the center of the circle
+        pixelValue = impixel(videoFrame, centers(1), centers(2))./255;
+            
+        % find the string name for the color
+        color = rgb2name(pixelValue);
+            
+        % draw a white circle over the circle detected
+        videoFrame = insertShape(videoFrame, 'Filledcircle', [centers(1, :), radii(1, :)], 'color', 'white',...
+                                      'opacity', 0.4);
+            
+        % calculate the area of the circle detected (in pixels)
+        area = pi*radii(1)^2;
+            
+        % conver the area to a string value (to communicate with ROS)
+        str_area = num2str(area);
+            
+        % set up a message for the publisher
+        msg = rosmessage(testpub);
+            
+        % give the message the string that needs to be sent
+        msg.Data = [color str_area];
+            
+        % send the message
+        send(testpub,msg);
 
-     end
+        display([color ' ' str_area]);
+    end
 
     % Display the annotated video frame using the video player object.
     step(videoPlayer, videoFrame);
@@ -125,5 +93,3 @@ end
 % Clean up.
 clear cam;
 release(videoPlayer);
-release(pointTracker);
-release(blobDetector);
