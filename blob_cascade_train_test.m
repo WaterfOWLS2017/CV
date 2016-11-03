757196605f1bd1b424a63989cacb855ed6e595cb
Gabriella G. Suarez <tuf00573@temple.edu>
2016-11-03 11:12:07 -0400

    added more nonbuoy pictures

--- blob_cascade_train_test.m
+++ blob_cascade_train_test.m
@@ -15,7 +15,7 @@ load('buoyLabelingSession.mat');
 %% load in the camera data
 % load in the bumblebee2 data
 % 
-vid = videoinput('pointgrey', 1, 'F7_BayerBG8_640x480_Mode3');
+%vid = videoinput('pointgrey', 1, 'F7_BayerBG8_640x480_Mode3');
 
 
 %%
@@ -30,41 +30,49 @@ negativeFolder = 'non_colored_buoys/'; %fullfile(matlabroot,'toolbox','vision','
 negativeImages = imageDatastore(negativeFolder);
 %%
 % Train a cascade object detector called 'buoyDetector.xml' using HOG features. The following command may take several minutes to run:
-trainCascadeObjectDetector('buoyDetector.xml', positiveInstances, negativeFolder, 'FalseAlarmRate', 0.01, 'NumCascadeStages', 5);
+trainCascadeObjectDetector('buoyDetector.xml', positiveInstances, negativeFolder, 'FalseAlarmRate', 0.001, 'NumCascadeStages', 4);
 
 %%
 % Use the newly trained classifier to detect a buoy in an image.
 detector = vision.CascadeObjectDetector('buoyDetector.xml'); 
 
-image = getsnapshot(vid);
+%image = getsnapshot(vid);
 
-[m n] = size(image);
+%[m n] = size(image);
 %imshow(image);
 
 %%
-%contents = dir('yellow_buoys');
-%for i=4:21
-while (1)   
-    image = getsnapshot(vid);
-    image = image(:,1:(n/6),:);
-    imshow(image);
-    pause;
+contents = dir('red_buoys');
+for i=4:21
+%while (1)   
+%    image = getsnapshot(vid);
+%    image = image(:,1:(n/6),:);
+%    imshow(image);
+%1
+
     % Read the test image.
-    %image = imread(['yellow_buoys/' contents(i).name]);
-  
+    image = imread(['red_buoys/' contents(i).name]);
+    [row, col] = size(image);
+    col = col/3;
     % Detect a stop sign.
     bbox = step(detector,image);
     [m,n] = size(bbox);
-%     detectedImg = image;
-%     for i=1:m
-%        xCenter = round(bbox(i,1)+ bbox(i,3)/2);
-%        yCenter = round(bbox(i,2)+ bbox(i,4)/2);
-%        pixels = impixel(image, xCenter, yCenter);
-%        detectedImg = insertObjectAnnotation(detectedImg,'rectangle',bbox(i,:),['buoy' num2str(pixels)]);
-%     end
+    detectedImg = image;
+     for j=1:m
+        xCenter = round(bbox(j,1)+ (bbox(j,3)/2));
+        yCenter = round(bbox(j,2)+ (bbox(j,4)/2));
+        display(xCenter);
+        display(yCenter);
+        ycoord = ((col/2) - yCenter)/(col/2);
+        xcoord = -1*((row/2) - xCenter)/(row/2);
+        display(xcoord);
+        display(ycoord);
+        pixels = impixel(image, xCenter, yCenter);
+        detectedImg = insertObjectAnnotation(detectedImg,'rectangle',bbox(j,:),['buoy' num2str(pixels)]);
+     end
     
     % Insert bounding boxes and return marked image.
-     detectedImg = insertObjectAnnotation(image,'rectangle',bbox,'buoy');
+    % detectedImg = insertObjectAnnotation(detectedImg,'rectangle',bbox,'buoy');
      
     
     % Display the detected stop sign.
